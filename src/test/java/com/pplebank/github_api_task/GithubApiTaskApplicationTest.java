e895f53266dca73ff95c142ee70b267c8d4fe43e
Piotr <pplebank@gmail.com>
2023-06-08 19:11:48 +0200

    added 2 more tests, changed json resources

--- src/test/java/com/pplebank/github_api_task/GithubApiTaskApplicationTest.java
+++ src/test/java/com/pplebank/github_api_task/GithubApiTaskApplicationTest.java
@@ -2,6 +2,8 @@ package com.pplebank.github_api_task;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.pplebank.github_api_task.Entity.GithubRepository;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
@@ -15,14 +17,21 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GithubApiTaskApplicationTest {
-    private WebTestClient webTestClient;
+    private static WebTestClient webTestClient;
+    private static WireMockServer wireMockServer;
 
-    @Test
-    public void testListRepositories() {
-        var wireMockServer = new WireMockServer();
+
+    @BeforeAll
+    public static void setup(){
+        wireMockServer = new WireMockServer();
         wireMockServer.start();
 
-        stubFor(get(urlEqualTo("/users/pplebank/repos"))
+        webTestClient = WebTestClient.bindToServer()
+                .baseUrl("http://localhost:" + wireMockServer.port())
+                .build();
+
+        stubFor(get(urlEqualTo("/users/existing_user/repos"))
+                .withHeader("Accept", containing("application/json"))
                 .willReturn(aResponse()
                         .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                         .withBodyFile("repositories.json")));
@@ -31,20 +40,42 @@ public class GithubApiTaskApplicationTest {
                 .willReturn(aResponse()
                         .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                         .withBodyFile("branches.json")));
-
-
-        webTestClient = WebTestClient.bindToServer()
-                .baseUrl("http://localhost:" + wireMockServer.port())
-                .build();
-
-        webTestClient.get()
-                .uri("/users/pplebank/repos")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBodyList(GithubRepository.class);
-        wireMockServer.stop();
-
     }
 
+    @AfterAll
+    public static void teardown() {
+        wireMockServer.stop();
+    }
+    @Test
+    public void testListRepositoriesSuccess() {
+
+        webTestClient.get()
+                .uri("/users/existing_user/repos")
+                .header("Accept", "application/json")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(GithubRepository.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void testListRepositoriesNoUserFailed() {
+
+        webTestClient.get()
+                .uri("/users/non_existing_user/repos")
+                .header("Accept", "application/json")
+                .exchange()
+                .expectStatus().is4xxClientError();
+    }
+
+    @Test
+    public void testListRepositoriesNoJsonHeaderFailed() {
+
+        webTestClient.get()
+                .uri("/users/existing_user/repos")
+                .header("Accept", "application/xml")
+                .exchange()
+                .expectStatus().is4xxClientError();
+    }
 
 }
